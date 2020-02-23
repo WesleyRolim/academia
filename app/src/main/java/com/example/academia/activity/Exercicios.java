fd5f6eec1ce556bb5b9f9c872e09129417464bcb
WesleyRolim <wesley.rolims@gmail.com>
2020-02-22 23:10:19 -0300

    criando a exercicio

---
+++ app/src/main/java/com/example/academia/activity/Exercicios.java
@@ -0,0 +1,24 @@
+package com.example.academia.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+
+import com.example.academia.R;
+
+
+public class Exercicios extends AppCompatActivity {
+
+    private Spinner musculos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercicios);
+
+        musculos = findViewById(R.id.gupoMuscularesSpinner);
+
+
+    }
+}
