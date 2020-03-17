b9a183977f8222a502337425ac3f426fa913ac83
WesleyRolim <wesley.rolims@gmail.com>
2020-03-17 18:33:43 -0300

    Foi criado a activity da ficha do aluno

---
+++ app/src/main/java/com/example/academia/activity/FichaAluno.java
@@ -0,0 +1,18 @@
+package com.example.academia.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.academia.R;
+
+public class FichaAluno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ficha_aluno);
+
+        
+    }
+}
